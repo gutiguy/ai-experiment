b234712a8ccd4092afefa32dc24d855a572ccd93
gutiguy <gutiguy.dev@gmail.com>
2023-04-01 19:39:18 +0300

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Typography, Box } from "@mui/material"
+
+function App() {
+
+  return (
+    <div>
+      <Box>
+        <Typography variant="h1">Hello World</Typography>
+      </Box>
+    </div>
+  )
+}
+
+export default App
